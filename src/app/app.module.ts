66096c017bf1b9dea8b5790ceac51ddff0cbf59f
Sergio Fernando Marentes Jiménez <85042036+SergioMarentesJ@users.noreply.github.com>
2023-05-28 14:58:38 -0600

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+import { CommonModule } from '@angular/common';
+
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+
+import { PagesModule } from './pages/pages.module';
+import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Calendar, CalendarModule } from 'primeng/calendar';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CommonModule,
+    AppRoutingModule,
+    RouterModule,
+    SharedModule,
+    PagesModule,
+    NgbModule,
+    CalendarModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
